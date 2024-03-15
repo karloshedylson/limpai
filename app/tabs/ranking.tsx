27bca9a3db893bb6cbfc7fa69f1888597fc2da1d
karloshedylson <khedylson@gmail.com>
2024-03-15 00:01:42 -0300

    :hammer: Add bottom navigation bar and date inline picker with list of chores

---
+++ app/tabs/ranking.tsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router';
+import { View, Text } from 'react-native';
+
+export default function Tab() {
+  return (
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+      <Text>ranking</Text>
+      <Stack.Screen options={{ headerShown: false }}/> 
+    </View>
+  );
+}
